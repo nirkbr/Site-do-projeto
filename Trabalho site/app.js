6c4cd5e23fb46f7b5ba5a3d926cf11b02f7aef5f
nirkbr <139012728+nirkbr@users.noreply.github.com>
2023-09-27 08:45:46 -0300

    Add files via upload

---
+++ Trabalho site/app.js
@@ -0,0 +1,47 @@
+function modulo(number, mod) {
+  let result = number % mod;
+  if (result < 0) {
+    result += mod;
+  }
+  return result;
+}
+
+class Carousel {
+  constructor(carousel) {
+    // find elements
+    this.carousel = carousel;
+    this.buttonPrevious = carousel.querySelector('[data-carousel-button-previous]');
+    this.buttonNext = carousel.querySelector('[data-carousel-button-next]');
+    this.slidesContainer = carousel.querySelector('[data-carousel-slides-container]');
+
+    // state
+    this.currentSlide = 0;
+    this.numSlides = this.slidesContainer.children.length;
+
+    // add events
+    this.buttonPrevious.addEventListener('click', this.handlePrevious.bind(this));
+    this.buttonNext.addEventListener('click', this.handleNext.bind(this));
+  }
+
+  handleNext() {
+    this.currentSlide = modulo(this.currentSlide + 1, this.numSlides);
+    this.carousel.style.setProperty('--current-slide', this.currentSlide);
+  }
+
+  handlePrevious() {
+    this.currentSlide = modulo(this.currentSlide - 1, this.numSlides);
+    this.carousel.style.setProperty('--current-slide', this.currentSlide);
+  }
+}
+
+const carousels = document.querySelectorAll('[data-carousel]');
+carousels.forEach(carousel => new Carousel(carousel));
+
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar_Itens')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar_Link-toggle')
+  .addEventListener('click', classToggle);
